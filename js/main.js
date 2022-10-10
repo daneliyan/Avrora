33e53638ef4782c636c93c8bf304c7e8b1f88d4d
daneliyan <105802998+daneliyan@users.noreply.github.com>
2022-10-10 20:47:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".team-slider", {
+  slidesPerView: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
